02bf41a6b7b8f8fd9d6b785e8a5308d69ddd416d
Ryan F. Salerno <rfsalerno@gmail.com>
2015-02-19 01:32:53 +0000

    refactored

--- test/widget_currency_spec.js
+++ test/widget_currency_spec.js
@@ -1,23 +1,58 @@
-/* global describe it expect */
+var WidgetCurrency = function(base, amount){
+    this.base = base || "foo";
+    this.amount = amount || 0;
+    this.display = function(){
+        return this.base + " | " + this.amount;
+    }
+}
 
-var widgetcurrency = function(){
-    this.base = "foo";
-    this.amount = 0;
+WidgetCurrency.prototype.display = function(){
+    return this.base + " | " + this.amount;
 };
 
-describe("widgetcurrency", function () {
-    it("is definied", function(){
-        expect(widgetcurrency).toBeDefined();
+describe("WidgetCurrency", function(){
+    it("is defined", function(){
+        expect(WidgetCurrency).toBeDefined;
     });
-    describe("defaults", function(){
-       it("base is foo", function(){
-          var currency = new widgetcurrency();
-          console.log(currency);
-          expect(currency.base).toEqual("foo");
-       });
-       it("amount is 0", function(){
-          var currency = new widgetcurrency();
-          expect(currency.amount).toEqual(0);
-       });
+    
+    describe("defaults", function() {
+        var currency;
+        
+        beforeEach(function(){
+            currency = new WidgetCurrency();
+        })
+        
+        it("base is foo", function() {
+            var currency = new WidgetCurrency();
+            console.log(currency)
+            expect(currency.base).toEqual("foo");
+        });
+        it("amount is 0", function(){
+            var currency = new WidgetCurrency();
+            expect(currency.amount).toEqual(0);
+        });
     });
+    
+    describe("setting svalues with constructor", function() {
+        var currency;
+        
+        beforeEach(function(){
+            currency = new WidgetCurrency("bar")
+        })
+        // because we now set this before, we no longer need the new instance of WidgetCurrency
+        it("base can be set to bar", function() {
+            // var currency = new WidgetCurrency("bar");
+            expect(currency.base).toEqual("bar");
+        });
+    
+    });
+    
+    describe("display", function() {
+        describe("when base is buzz and amount is 10", function() {
+            it("is 'buzz | 10'", function() {
+                var currency = new WidgetCurrency("buzz", 10);
+                expect(currency.display()).toEqual('buzz | 10')
+            })
+        })
+    })
 });
